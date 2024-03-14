396749c564c4eb4b1c005d14c1a252b96d4aaf3d
Nucliam <liam.anthian@gmail.com>
2024-03-14 15:10:44 +1100

    Optimised Dijkstra's a little, and fixed file reading

--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,19 @@
+# --- Imports ---
+from pathlib import Path
+
+# --- Constants ---
+FILENAME = 'paths.txt'
+
 def main():
     # = Open and read file =
-    fp = open("paths.txt")
+    file = Path(__file__).with_name(FILENAME)
+    fp = open(file, "r")
+
     start = fp.readline().strip('\n')
     goal = fp.readline().strip('\n')
 
-    paths = {}
-    queue = [([start],0)]
-
     # - Assemble path dictionary -
+    paths = {}
     for line in fp.readlines():
         (a, b, c) = line.strip('\n').split(',')
         cost = int(c)
@@ -19,22 +25,34 @@ def main():
         elif paths[a][b] > cost : paths[a][b] = cost
 
 
+    # = Searching algorithm =
+    queue = [([start],0)]
+    seen = []
+
     # - Find path from `start` to `goal` via Dijkstra's algorithm -
     while len(queue) > 0:
         # Pop first element in queue and add children
-        (path, c) = queue.pop(0)
+        (p, c) = queue.pop(0)
+        curr = p[-1]
 
-        if path[-1] == goal:
+        if curr == goal:
             # end state reached!
-            print(F"Path: {path}, Cost: {c}")
+            print(F"Path: {p}, Cost: {c}")
+            fp.close()
             return 0
+        # Drop nodes already explored by some cheaper path
+        elif curr in seen:
+            continue
+        else:
+            # otherwise add node to list of seen nodes
+            seen.append(curr)
         
         # Add possible paths to queue
-        if path[-1] in paths.keys():
-            s = paths[path[-1]]
-            for (k,v) in s.items():
+        if curr in paths.keys():
+            for (k,v) in paths[curr].items():
                 # Insert new nodes into list, sorted by ascending pathcost
-                queue.insert(1, (path + [k], c+v))
+                # could optimise futher here by checking `seen` list... 
+                queue.insert(1, (p + [k], c+v))
         else:
             # no known paths for location - dead node
             continue
@@ -45,6 +63,7 @@ def main():
 
     # if code reaches this point, no valid paths to goal state
     print(F"No path found from {start} to {goal}!")
+    fp.close()
     return -1
 
 main()
